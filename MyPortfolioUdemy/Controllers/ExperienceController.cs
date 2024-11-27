7f3ca3726cb28549ceff6740667bd821b2d050dd
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-11-27 18:59:41 +0300

    @model List<Experience>  Yapmanın kolay yolu

--- MyPortfolioUdemy/Controllers/ExperienceController.cs
+++ MyPortfolioUdemy/Controllers/ExperienceController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
 
 namespace MyPortfolioUdemy.Controllers
 {
     public class ExperienceController : Controller
     {
+        MyPortfolioContext context = new MyPortfolioContext();
         public IActionResult ExperienceList()
         {
-            return View();
+            var values = context.Experiences.ToList();
+            return View(values);
         }
     }
 }
