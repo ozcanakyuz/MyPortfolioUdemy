217506cc4554a360defe8f0467b7c4e827cc953f
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-11-27 20:16:56 +0300

    HttpGet and HttpPost CreateExperience DeleteExperience UpdateExperience OK

--- MyPortfolioUdemy/Controllers/ExperienceController.cs
+++ MyPortfolioUdemy/Controllers/ExperienceController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyPortfolioUdemy.DAL.Context;
+using MyPortfolioUdemy.DAL.Entities;
 
 namespace MyPortfolioUdemy.Controllers
 {
@@ -10,6 +11,58 @@ namespace MyPortfolioUdemy.Controllers
         {
             var values = context.Experiences.ToList();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            if (!ModelState.IsValid) // Doğrulama başarısızsa
+            {
+                // Hata mesajları ile birlikte formu tekrar göster
+                return View(experience);
+            }
+
+            // Doğrulama başarılıysa veritabanına kaydet
+            context.Experiences.Add(experience);
+            context.SaveChanges();
+
+            return RedirectToAction("ExperienceList");
+        }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var value = context.Experiences.Find(id);
+            context.Experiences.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var value = context.Experiences.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            if (!ModelState.IsValid) // Doğrulama başarısızsa
+            {
+                // Hata mesajları ile birlikte formu tekrar göster
+                return View(experience);
+            }
+
+            // Doğrulama başarılıysa veritabanına kaydet
+            context.Experiences.Update(experience);
+            context.SaveChanges();
+
+            return RedirectToAction("ExperienceList");
         }
     }
 }
